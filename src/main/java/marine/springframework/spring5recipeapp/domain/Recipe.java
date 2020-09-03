c89c78bb2b5b10903faebbf45f96e4bc04280b0b
MinhTri1401 <minhtri1412000@gmail.com>
2020-09-03 22:32:40 +0700

    Pro-Tips - Using Setters for JPA Bidirectional Relationships

--- src/main/java/marine/springframework/spring5recipeapp/domain/Recipe.java
+++ src/main/java/marine/springframework/spring5recipeapp/domain/Recipe.java
@@ -117,6 +117,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+        notes.setRecipe(this);
+    }
+
+    public Recipe addIngredients(Ingredient ingredient){
+        ingredient.setRecipe(this);
+        this.ingredients.add(ingredient);
+        return this;
     }
 
     public Set<Ingredient> getIngredients() {
@@ -142,4 +149,5 @@ public class Recipe {
     public void setCategories(Set<Category> categories) {
         this.categories = categories;
     }
+
 }
